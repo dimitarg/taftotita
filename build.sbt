a7e4b321b86e2023448121028357bf68a9f3cc20
Dimitar Georgiev <dimitar.georgiev.bg@gmail.com>
2024-10-30 15:02:11 +0200

    Sample scala 3 starter

---
+++ build.sbt
@@ -0,0 +1,8 @@
+
+ThisBuild / scalaVersion := "3.3.4"
+
+
+libraryDependencies ++= Seq(
+  "co.fs2" %% "fs2-io" % "3.11.0",
+  "io.github.dimitarg"  %%  "weaver-test-extra" % "0.5.11" % "test"
+)
---
+++ src/test/scala/example/ExampleSuite.scala
@@ -0,0 +1,15 @@
+package example
+
+import cats.implicits.*
+import cats.effect.*
+import fs2.Stream
+import weaver.pure.*
+
+object ExampleSuite extends Suite:
+
+  override def suitesStream: fs2.Stream[IO, Test] = Stream(
+    pureTest("example test") {
+      expect(1 === 1)
+    }
+  )
+end ExampleSuite
