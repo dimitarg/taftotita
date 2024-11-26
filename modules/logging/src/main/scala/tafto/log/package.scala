4f88d4447de90dd1cf0e07acabb74aa92a155262
Dimitar Georgiev <dimitar.georgiev.bg@gmail.com>
2024-11-26 19:47:39 +0200

    Stray debug

--- modules/logging/src/main/scala/tafto/log/package.scala
+++ modules/logging/src/main/scala/tafto/log/package.scala
@@ -4,4 +4,4 @@ import cats.effect.*
 import io.odin.*
 
 package object log:
-  def defaultLogger[F[_]: Sync]: Logger[F] = consoleLogger[F](minLevel = Level.Debug)
+  def defaultLogger[F[_]: Sync]: Logger[F] = consoleLogger[F](minLevel = Level.Info)
