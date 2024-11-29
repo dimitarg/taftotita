0cbdc13ed6bc8e3a6c5242c8fae4acbccff1f98d
Dimitar Georgiev <dimitar.georgiev.bg@gmail.com>
2024-11-29 19:57:19 +0200

    Decrease lock waits in case of multiple consumers

--- modules/persist/src/main/scala/tafto/persist/PgEmailMessageRepo.scala
+++ modules/persist/src/main/scala/tafto/persist/PgEmailMessageRepo.scala
@@ -47,44 +47,42 @@ final case class PgEmailMessageRepo[F[_]: Clock: MonadCancelThrow](
   }
 
   override def claim(id: EmailMessage.Id): F[Option[EmailMessage]] =
-    updateStatusReturning(id = id, status = EmailStatus.Claimed, previousStatus = EmailStatus.Scheduled)
+    Time[F].utc.flatMap { now =>
+      updateStatusReturning(UpdateStatus.claim(id, now))
+    }
 
   override def markAsSent(id: EmailMessage.Id): F[Boolean] =
-    updateStatus(id = id, status = EmailStatus.Sent, previousStatus = EmailStatus.Claimed)
+    Time[F].utc.flatMap { now =>
+      updateStatus(UpdateStatus.markAsSent(id, now))
+    }
 
   override def markAsError(id: EmailMessage.Id, error: String): F[Boolean] = for
     now <- Time[F].utc
     result <- database.pool.use { s =>
       for
         command <- s.prepare(EmailMessageQueries.updateStatusAndError)
-        completion <- command.execute((EmailStatus.Error, now, error, id, EmailStatus.Claimed))
+        completion <- command.execute((UpdateStatus.markAsError(id, now), error))
         result = wasUpdated(completion)
       yield result
     }
   yield result
 
-  private def updateStatus(id: EmailMessage.Id, status: EmailStatus, previousStatus: EmailStatus): F[Boolean] = for
-    now <- Time[F].utc
-    result <- database.pool.use { s =>
+  private def updateStatus(updateStatus: UpdateStatus): F[Boolean] = for result <- database.pool.use { s =>
       for
         command <- s.prepare(EmailMessageQueries.updateStatus)
-        completion <- command.execute((status, now, id, previousStatus))
+        completion <- command.execute(updateStatus)
         result = wasUpdated(completion)
       yield result
     }
   yield result
 
-  private def updateStatusReturning(
-      id: EmailMessage.Id,
-      status: EmailStatus,
-      previousStatus: EmailStatus
-  ): F[Option[EmailMessage]] =
+  private def updateStatusReturning(updateStatus: UpdateStatus): F[Option[EmailMessage]] =
     for
       now <- Time[F].utc
       result <- database.pool.use { s =>
         for
           query <- s.prepare(EmailMessageQueries.updateStatusReturning)
-          result <- query.option((status, now, id, previousStatus))
+          result <- query.option(updateStatus)
         yield result
       }
     yield result
@@ -135,17 +133,67 @@ object EmailMessageQueries {
   """.query(emailMessageId)
 
   val updateStatus = sql"""
-    update email_messages set status=${emailStatus}, last_attempted_at=${timestamptz} where id=${emailMessageId} and status=${emailStatus};
-  """.command
+    with ids as (
+      select id from email_messages where id=${emailMessageId} and status=${emailStatus}
+      for update skip locked
+    )
+    update email_messages m set status=${emailStatus}, last_attempted_at=${timestamptz}
+    from ids
+    where m.id = ids.id;
+  """.command.contramap[UpdateStatus] { updateStatus =>
+    (updateStatus.id, updateStatus.currentStatus, updateStatus.newStatus, updateStatus.updatedAt)
+  }
 
   val updateStatusAndError = sql"""
-    update email_messages set status=${emailStatus}, last_attempted_at=${timestamptz}, error=${text}
-    where id=${emailMessageId} and status=${emailStatus};
-  """.command
+    with ids as (
+      select id from email_messages where id=${emailMessageId} and status=${emailStatus}
+      for update skip locked
+    )
+    update email_messages m set status=${emailStatus}, last_attempted_at=${timestamptz}, error=${text}
+    from ids
+    where m.id = ids.id;
+  """.command.contramap[(UpdateStatus, String)] { (updateStatus, errorMessage) =>
+    (updateStatus.id, updateStatus.currentStatus, updateStatus.newStatus, updateStatus.updatedAt, errorMessage)
+  }
 
   val updateStatusReturning = sql"""
-    update email_messages set status=${emailStatus}, last_attempted_at=${timestamptz} where id=${emailMessageId} and status=${emailStatus}
+    with ids as (
+      select id from email_messages where id=${emailMessageId} and status=${emailStatus}
+      for update skip locked
+    )
+    update email_messages m set status=${emailStatus}, last_attempted_at=${timestamptz}
+    from ids
+    where m.id = ids.id
     returning subject, to_, cc, bcc, body;
-  """.query(domainEmailMessageCodec)
+  """.query(domainEmailMessageCodec).contramap[UpdateStatus] { updateStatus =>
+    (updateStatus.id, updateStatus.currentStatus, updateStatus.newStatus, updateStatus.updatedAt)
+  }
 
 }
+
+final case class UpdateStatus(
+    id: EmailMessage.Id,
+    currentStatus: EmailStatus,
+    newStatus: EmailStatus,
+    updatedAt: OffsetDateTime
+)
+
+object UpdateStatus:
+  def claim(id: EmailMessage.Id, updatedAt: OffsetDateTime) = UpdateStatus(
+    id = id,
+    currentStatus = EmailStatus.Scheduled,
+    newStatus = EmailStatus.Claimed,
+    updatedAt = updatedAt
+  )
+  def markAsSent(id: EmailMessage.Id, updatedAt: OffsetDateTime) = UpdateStatus(
+    id = id,
+    currentStatus = EmailStatus.Claimed,
+    newStatus = EmailStatus.Sent,
+    updatedAt = updatedAt
+  )
+  def markAsError(id: EmailMessage.Id, updatedAt: OffsetDateTime) = UpdateStatus(
+    id = id,
+    currentStatus = EmailStatus.Claimed,
+    newStatus = EmailStatus.Error,
+    updatedAt = updatedAt
+  )
